89f7aa75f02a45dd43a4ca5410f15b5a5d71fc51
Akchhay Alok <akchhay.alok91@gmail.com>
2020-06-12 16:36:33 +0530

    CI/CD

---
+++ deploy.groovy
@@ -0,0 +1,18 @@
+def toKubernetes(tagToDeploy, namespace, 
+➥deploymentName) {    
+  sh("sed -i.bak 's#BUILD_TAG#${tagToDeploy}#' ./deploy/${namespace}/*.yml")
+
+  container('kubectl') {
+    kubectl("apply -f deploy/${namespace}/")
+  }
+}
+def kubectl(namespace, command) {    
+ sh("kubectl --namespace=${namespace} ${command}")    
+} 
+
+def rollback(deploymentName) {
+  kubectl("rollout undo deployment/${deploymentName}")
+}
+
+return this;
+
